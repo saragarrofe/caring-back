93de53465c33ebbc892a022932c05438b64029ae
Sara Garrofé <s.garrofe@gmail.com>
2024-11-04 21:55:41 +0100

    Bug fixed in auth router

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,17 @@
-import { Router } from 'express';
+// src/routes/auth.routes.ts
+import { Router, Request, Response, NextFunction } from 'express';
 import { login, register } from '../controllers/auth.controller';
 
 const authRoutes: Router = Router();
 
-authRoutes.post('/login', login);
-authRoutes.post('/register', register);
+authRoutes.post('/login', (req: Request, res: Response, next: NextFunction) => {
+    login(req, res, next)
+        .catch((err) => res.status(500).json({ message: 'Internal server error' }));
+});
 
-export default authRoutes; 
+authRoutes.post('/register', (req: Request, res: Response,  next: NextFunction) => {
+    register(req, res, next)
+        .catch((err) => res.status(500).json({ message: 'Internal server error' }));
+});
+
+export default authRoutes;
