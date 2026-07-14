bbe2f7982d7f425a299a60e047a2a40a36d61d3f
Sara Garrofé <s.garrofe@gmail.com>
2026-07-14 07:32:08 +0200

    feat: implement update and delete functionality and enhance plant routes

--- src/controllers/plant.controller.ts
+++ src/controllers/plant.controller.ts
@@ -25,3 +25,45 @@ export const createPlant = async (req: Request, res: Response) => {
         return res.status(500).json({ message: 'Internal server error' });
     }
 }
+
+export const updatePlant = async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
+        const plantRepository = AppDataSource.getRepository(Plant); // objeto que TypeORM da para hacer operaciones sobre una tabla específica
+        const plant = await plantRepository.findOneBy({ id: Number(id), user: { id: req.user?.id } });
+        
+        if(!plant) {
+            return res.status(404).json({ message: 'Plant not found' });
+        }
+        
+        const updatePlant =  Object.assign(plant, req.body)
+        const savedPlant = await plantRepository.save(updatePlant);
+
+        return res.json(savedPlant)
+        
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+export const deletePlant = async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
+        const plantRepository = AppDataSource.getRepository(Plant);
+        const plant = await plantRepository.findOneBy({ id: Number(id), user: {id: req.user?.id}});
+
+        if(!plant) {
+            return res.status(404).json({ message: 'Plant not found' });
+        }
+
+        await plantRepository.delete(Number(id))
+
+        return res.status(204).send()
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Internal server error' });
+        
+    }
+}
--- src/routes/plant.routes.ts
+++ src/routes/plant.routes.ts
@@ -1,11 +1,13 @@
 import { Router, Request, Response, NextFunction } from 'express';
-import { createPlant, getPlants } from '../controllers/plant.controller';
+import { createPlant, deletePlant, getPlants, updatePlant } from '../controllers/plant.controller';
 import { verifyToken } from '../middlewares/auth.middleware';
 
 const plantRoutes: Router = Router();
 
 plantRoutes.get('/', verifyToken, getPlants);
-  plantRoutes.post('/', verifyToken, createPlant);
+plantRoutes.post('/', verifyToken, createPlant);
+plantRoutes.patch('/:id', verifyToken, updatePlant);
+plantRoutes.delete('/:id', verifyToken, deletePlant)
 
 export default plantRoutes;
    
